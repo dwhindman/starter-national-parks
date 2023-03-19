af04c50a08ab4b15c3a97b5e430ef7be7cf7620e
David Hindman <david.w.hindman@gmail.com>
2023-03-19 10:14:20 -0400

    Form submissions module

--- index.js
+++ index.js
@@ -1,1 +1,106 @@
+const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const form = event.target;
+    const formData = new FormData(form);
+    const errors = validateForm(formData);
 
+    //clear previous errors
+    const errorElements = document.querySelectorAll(".error");
+    for (let element of errorElements){
+        element.style.display = "none";
+    };
+
+    //Display any new errors
+    Object.keys(errors).forEach((key) => {
+        //Find the specific error element
+        const errorElement = document.querySelector(`#${key}-form .error`);
+        errorElement.innerHTML = errors[key];
+        errorElement.style.display = "block";
+    });
+
+    //if no errors
+    if(!Object.keys(errors).length){
+        //create new element
+        const parkSection = document.createElement("section");
+        //add park class
+        parkSection.classList.add("park-display");
+        //Construct HTML for new element
+        const content = `
+        <h2>${formData.get("name")}</h2>
+        <div class="location-display">${formData.get("location")}</div>
+        <div class="description-display">${formData.get("description")}</div>
+        <button class="rate-button" title="Add to Favourites">&#9734;</button>
+        <div class="stats">
+          <div class="established-display stat">
+            <h3>Established</h3>
+            <div class="value">${moment(formData.get("established")).format(
+              "MMMM D, YYYY"
+            )}</div>
+          </div>
+          <div class="area-display stat">
+            <h3>Area</h3>
+            <div class="value">${formData.get("area")}</div>
+          </div>
+          <div class="rating-display stat">
+            <h3>Rating</h3>
+            <div class="value">${formData.get("rating")}</div>
+          </div>
+        </div>
+        `;
+        //Set innerHTML
+        parkSection.innerHTML = content;
+        //append to main
+        document.querySelector("main").appendChild(parkSection);
+    }
+};
+
+const main = () => {
+    //Get form element
+    const form = document.querySelector("#park-form");
+
+    //Attach submit handler
+    form.addEventListener("submit", submitHandler);
+};
+
+function validateExists(value) {
+    return value && value.trim();
+}
+
+function validateForm(formData) {
+    const errors = {};
+  
+    // Check if name was entered
+    if (!validateExists(formData.get("name"))) {
+      errors.name = "Please enter a name";
+    }
+  
+    // Check if rating was entered
+    if (!validateExists(formData.get("rating"))) {
+      errors.rating = "Please enter a rating";
+    }
+  
+    // Check if description was entered
+    if (!validateExists(formData.get("description"))) {
+      errors.description = "Please enter short description";
+    }
+  
+    // Check if established date was entered
+    if (!validateExists(formData.get("established"))) {
+      errors.established = "Please enter date";
+    }
+  
+    // Check if area was entered
+    if (!validateExists(formData.get("area"))) {
+      errors.area = "Please enter the area of the park";
+    }
+  
+    // Check if location date was entered
+    if (!validateExists(formData.get("location"))) {
+      errors.location = "Please enter the location of the park";
+    }
+  
+    return errors;
+  }
+
+window.addEventListener("DOMContentLoaded", main);
